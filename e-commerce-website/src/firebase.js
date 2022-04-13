89520c26f9a3bbdf202424301b872b06f4b77f8f
Jaymin Patil <jayminpatil123@gmail.com>
2022-04-13 17:22:55 +0530

    SettingupApp

---
+++ e-commerce-website/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCM9d5VDzSL8T9sjviqZkvrP8OYP6kWXXA",
+    authDomain: "e-commerce-website-77f05.firebaseapp.com",
+    projectId: "e-commerce-website-77f05",
+    storageBucket: "e-commerce-website-77f05.appspot.com",
+    messagingSenderId: "567691423972",
+    appId: "1:567691423972:web:ca7f8fc1ba340ee233bcf1",
+    measurementId: "G-NTG5GCP6P9"
+  };
