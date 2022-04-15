5744317e96152a9deddb96747a009b3b3f555faa
Jaymin Patil <jayminpatil123@gmail.com>
2022-04-15 11:36:20 +0530

    headerUpdate

---
+++ e-commerce-website/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'> 
+        <img
+            className="Header_logo"
+            src=""
+        />   
+    </div>
+  )
+}
+
+export default Header
