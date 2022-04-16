bad40bc6f5d9d381d76512b7cd791a1c40f29bbd
Jaymin Patil <jayminpatil123@gmail.com>
2022-04-16 16:18:33 +0530

    homePage

--- e-commerce-website/src/Header.js
+++ e-commerce-website/src/Header.js
@@ -47,7 +47,7 @@ function Header() {
             
         </div>
     </div>
-  )
+  );
 }
 
 export default Header
---
+++ e-commerce-website/src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://scontent.fjai1-2.fna.fbcdn.net/v/t39.30808-6/277178260_7253118514729931_1136921847969073224_n.jpg?stp=dst-jpg_p640x640&_nc_cat=100&ccb=1-5&_nc_sid=e3f864&_nc_ohc=XLpCzXo5_cwAX_Y5kaO&_nc_ht=scontent.fjai1-2.fna&oh=00_AT8SlzbHIZg6tbEZfCTPM2rD9NsAED8fsSGkgJ2yXWlZ_Q&oe=625F7585"
+          alt=""
+        />
+      </div>
+    </div>
+    );
+};
+
+export default Home;
