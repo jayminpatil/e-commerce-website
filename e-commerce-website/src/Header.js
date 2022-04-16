ff01972ef0a9eb357479296868adbba3e0f41ac1
Jaymin Patil <jayminpatil123@gmail.com>
2022-04-16 15:31:59 +0530

    headerUpdate

--- e-commerce-website/src/Header.js
+++ e-commerce-website/src/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import MicIcon from '@mui/icons-material/Mic';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function Header() {
   return (
@@ -22,21 +23,28 @@ function Header() {
         <div className="header__nav">
             <div className='header__option'>
                 <span className='header__optionLineOne'>Hello Pal</span>
-                <span className='header__optionLineOne'>Sign In</span>
+                <span className='header__optionLineTwo'>Sign In</span>
             
             </div>
 
             <div className="header__option">
                 <span className='header__optionLineOne'>Orders</span>
-                <span className='header__optionLineOne'>& Returns</span>
+                <span className='header__optionLineTwo'>& Returns</span>
             
             </div>
 
             <div className="header__option">
                 <span className='header__optionLineOne'>Your</span>
-                <span className='header__optionLineOne'>Prime</span>
+                <span className='header__optionLineTwo'>Prime</span>
             
             </div>
+
+            <div className="header__optionBasket">
+                <ShoppingCartIcon className="header__shoppingCartIcon" />
+                <span className='header__optionLineTwo header__basketCount'>0</span>
+            
+            </div>
+            
         </div>
     </div>
   )
